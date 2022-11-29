e41625c959a4f3b234e0f247e1e9b06166bec803
marianacpais <76234277+marianacpais@users.noreply.github.com>
2022-11-29 14:47:48 +0000

    table update

--- codes_script/main.py
+++ codes_script/main.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import requests
 
-ds =  pd.read_excel("complete.xlsx")
+ds =  pd.read_excel("codes_script/table1_it2.xlsx")
 
-def get_code_description (code,code_sys,cm_bool=True):
+def get_code_description (id,code,code_sys,cm_bool=True):
     if cm_bool:
         code_sys = code_sys + "CM"
     code = str(code)
     url = 'https://uts-ws.nlm.nih.gov/rest/content/current/source/'+code_sys+'/' + code + '?apiKey=731f8791-8540-486f-bd3d-0289bc13b2f3'
-    x = requests.get(url).json()
     try:
-        print("Got description for " + code_sys +" code '"+code+"'")
+        x = requests.get(url).json()
+        print(str(id),": Got description for " + code_sys +" code '"+code+"'")
         return x["result"]["name"]
     except:
         return "ERROR"
@@ -46,28 +46,28 @@ def get_umls_cui (code,code_sys,cm_bool=True):
 
 # get_umls_cui ("J13","ICD10CM")
 
-def get_parents_ui (code,code_sys,cm_bool=True):
+def get_parents_ui (id,code,code_sys,cm_bool=True):
     if cm_bool:
         code_sys = code_sys + "CM"
     code = str(code)
     url = 'https://uts-ws.nlm.nih.gov/rest/content/2022AB/source/'+code_sys+'/' + code + '/parents?apiKey=731f8791-8540-486f-bd3d-0289bc13b2f3'
-    x = requests.get(url).json()
     try:
-        print("Got parent ui for " + code_sys +" code '"+code+"'")
+        x = requests.get(url).json()
+        print(str(id)+": Got parent ui for " + code_sys +" code '"+code+"'")
         return x["result"][0]["ui"]
     except:
         return "ERROR"
 
-# get_parents_ui ("J13","ICD10")
+# get_parents_ui (1,"J13","ICD10")
 
-def get_parents_name (code,code_sys,cm_bool=True):
+def get_parents_name (id,code,code_sys,cm_bool=True):
     if cm_bool:
         code_sys = code_sys + "CM"
     code = str(code)
     url = 'https://uts-ws.nlm.nih.gov/rest/content/2022AB/source/'+code_sys+'/' + code + '/parents?apiKey=731f8791-8540-486f-bd3d-0289bc13b2f3'
-    x = requests.get(url).json()
     try:
-        print("Got parent name for " + code_sys +" code '"+code+"'")
+        x = requests.get(url).json()
+        print(str(id)+": Got parent name for " + code_sys +" code '"+code+"'")
         print(x["result"][0]["name"])
         return x["result"][0]["name"]
     except:
@@ -75,12 +75,12 @@ def get_parents_name (code,code_sys,cm_bool=True):
 
 # get_parents_name ("J13","ICD10")
 
-ds["code_description"] = ds.apply(lambda row: get_code_description(row['code'],row['original_code_system']), axis=1)
-ds["level1_code_description"] = ds.apply(lambda row: get_code_description(row['nearest_level1_code'],row['original_code_system']), axis=1)
+ds["code_description"] = ds.apply(lambda row: get_code_description(row['id'],row['code'],row['original_code_system']), axis=1)
+ds["level1_code_description"] = ds.apply(lambda row: get_code_description(row['id'],row['nearest_level1_code'],row['original_code_system']), axis=1)
 ds["umls_cui"] = ds.apply(lambda row: get_umls_cui(row['code'],row['original_code_system']), axis=1)
 ds["level1_umls_cui"] = ds.apply(lambda row: get_umls_cui(row['nearest_level1_code'],row['original_code_system']), axis=1)
-ds["parent_ui"] = ds.apply(lambda row: get_parents_ui(row['nearest_level1_code'],row['original_code_system']), axis=1)
-ds["parent_name"] = ds.apply(lambda row: get_parents_name(row['nearest_level1_code'],row['original_code_system']), axis=1)
+ds["parent_ui"] = ds.apply(lambda row: get_parents_ui(row['id'],row['nearest_level1_code'],row['original_code_system']), axis=1)
+ds["parent_name"] = ds.apply(lambda row: get_parents_name(row['id'],row['nearest_level1_code'],row['original_code_system']), axis=1)
 
 # Codes no CM
 # ds["code_description_woCM"] = ds.apply(lambda row: get_code_description(row['code'],row['original_code_system'],False), axis=1)
@@ -88,6 +88,14 @@ ds["parent_name"] = ds.apply(lambda row: get_parents_name(row['nearest_level1_co
 # ds["umls_cui_woCM"] = ds.apply(lambda row: get_umls_cui(row['code'],row['original_code_system'],False), axis=1)
 # ds["level1_umls_cui_woCM"] = ds.apply(lambda row: get_umls_cui(row['nearest_level1_code'],row['original_code_system'],False), axis=1)
 
-ds.to_csv('test.csv')
+ds.to_csv('FINAL_parent_name.csv')
 ds.to_json('test.json',orient='records')
 ds.to_html('test.html')
+
+code="A08.3"
+code_sys="ICD10"
+
+get_code_description (code,code_sys)
+get_umls_cui (code,code_sys)
+get_parents_ui (code,code_sys)
+get_parents_name (code,code_sys)
--- table_generator/main.py
+++ table_generator/main.py
@@ -1,4 +1,47 @@
 import pandas as pd
+import numpy as np
 
-table =  pd.read_excel("table.xlsx")
-table.to_html('test.html')
+table =  pd.read_excel("TABLE1_updated.xlsx")
+table.to_html('finalTable.html')
+table.to_json('finalTable.json',orient='records')
+
+# Filtrar por major_grouping_variable
+# Agrupar por artigo
+# Juntar códigos
+
+ds =  pd.read_excel("table_generator/finalTable.xlsx")
+
+majors = table.major_grouping_variable.unique()
+articles = table.article.unique()
+
+table = pd.DataFrame(
+    columns = articles,
+    index = majors
+)
+
+def format_code_list (codes):
+    formatted_codes = ""
+    for code in codes[:-1]:
+        formatted_codes = formatted_codes + str(code) + ", "
+    formatted_codes = formatted_codes + str(codes.iloc[-1])
+    return(formatted_codes)
+
+article = articles[10]
+major = majors[6]
+codes = ds[(ds.major_grouping_variable == major) & (ds.article == article)].code
+
+for major in majors:
+    print(major)
+    for article in articles:
+        codes = ds[(ds.major_grouping_variable == major) & (ds.article == article)].code
+        print(article)
+        if (len(codes) > 0):
+            formatted_codes = format_code_list (codes)
+            table.at[major, article] = formatted_codes
+        else:
+            table.at[major, article] = "-"
+
+table["Unnamed: 0"] = table["Unnamed: 0"].apply(lambda row: "<strong>"+str(row)+"</strong>")
+
+table.to_html('Table1FINAL.html', escape = False)
+table.to_csv('Table1FINAL.csv')
